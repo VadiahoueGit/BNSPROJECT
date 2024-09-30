089ec67d52cab90ba97bc0819d7a9d47d7da7cbb
karamoko vadiahoue <vadiahoue.karamoko@intuitcorporate.com>
2024-09-30 21:57:32 +0000

    forgot password

--- src/app/public/auth/auth-routing.module.ts
+++ src/app/public/auth/auth-routing.module.ts
@@ -2,10 +2,12 @@ import { RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { LogoutComponent } from "./logout/logout.component";
 import { NgModule } from "@angular/core";
+import { ForgotPasswordComponent } from "./forgot-password/forgot-password.component";
 
 const routes: Routes = [
     { path: '', component: LoginComponent },
-    { path: 'deconnexion', component: LogoutComponent }
+    { path: 'deconnexion', component: LogoutComponent },
+    { path: 'forgot-password', component: ForgotPasswordComponent}
   ];
   
   @NgModule({
--- src/app/public/auth/auth.module.ts
+++ src/app/public/auth/auth.module.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SharedComponentModule } from 'src/app/Features/shared-component/shared-component.module';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
-    LogoutComponent
+    LogoutComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     CommonModule,
