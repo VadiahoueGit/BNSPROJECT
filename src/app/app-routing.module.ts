2223cd9d0c401e1bd43129613df99247f1c68887
karamoko vadiahoue <vadiahoue.karamoko@intuitcorporate.com>
2024-09-30 10:58:21 +0000

    create dashboard module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/auth', pathMatch: 'full' },
   { path: 'auth', loadChildren: () => import('./public/auth/auth.module').then(m => m.AuthModule) },
   { path: 'clients', loadChildren: () => import('./Features/gestion-clients/gestion-clients.module').then(m => m.GestionClientsModule) },
+  { path: 'dashboard', loadChildren: () => import('./Features/dashboard/dashboard.module').then(m => m.DashboardModule) },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { AuthModule } from './public/auth/auth.module';
 import { GestionClientsModule } from './Features/gestion-clients/gestion-clients.module';
 import { SharedComponentModule } from './Features/shared-component/shared-component.module';
+import { DashboardModule } from './Features/dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { SharedComponentModule } from './Features/shared-component/shared-compon
   ],
   imports: [
     AuthModule,
+    DashboardModule,
     SharedComponentModule,
     GestionClientsModule,
     BrowserModule,
