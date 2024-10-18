9d0da24313931ed1ac368dae52e1e80822998242
karamoko vadiahoue <vadiahoue.karamoko@intuitcorporate.com>
2024-10-18 01:25:38 +0000

    type article OK

--- src/app/Features/parametre/type-article/type-article.component.ts
+++ src/app/Features/parametre/type-article/type-article.component.ts
@@ -1,10 +1,121 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ALERT_QUESTION } from '../../shared-component/utils';
+import { Table } from 'primeng/table';
+import { ArticleServiceService } from 'src/app/core/article-service.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-type-article',
   templateUrl: './type-article.component.html',
-  styleUrls: ['./type-article.component.scss']
+  styleUrls: ['./type-article.component.scss'],
 })
 export class TypeArticleComponent {
+  @ViewChild('dt2') dt2!: Table;
+  statuses!: any[];
+  dataList!: any[];
+  loading: boolean = true;
+  isModalOpen = false;
+  activityValues: number[] = [0, 100];
+  operation: string = '';
+  articleForm!: FormGroup;
+  isEditMode = false;
+  articleId: any;
+  updateData: any = {};
+  constructor(
+    private articleService: ArticleServiceService,
+    private _spinner: NgxSpinnerService,
+    private fb: FormBuilder
+  ) {}
 
+  ngOnInit() {
+    this.articleForm = this.fb.group({
+      libelle: ['', Validators.required],
+    });
+    this.GetTypesArticlesList();
+  }
+
+  clear(table: Table) {
+    table.clear();
+  }
+
+  OnCloseModal() {
+    this.isModalOpen = false;
+    console.log(this.isModalOpen);
+  }
+  OnCreate() {
+    this.isEditMode = false;
+    this.isModalOpen = true;
+    this.operation = 'create';
+    console.log(this.isModalOpen);
+  }
+
+  OnEdit(data: any) {
+    this.isEditMode = true;
+    console.log(data);
+    this.updateData = data;
+    this.articleId = data.id;
+    this.articleForm.patchValue(data);
+    this.isModalOpen = true;
+    this.operation = 'edit';
+    console.log(this.isModalOpen);
+  }
+  onSubmit(): void {
+    console.log(this.articleForm.value);
+    if (this.articleForm.valid) {
+      const formValues = this.articleForm.value;
+      this.articleForm.patchValue(this.updateData);
+      if (this.isEditMode) {
+        this.articleService.updateArticle(this.articleId, formValues).then(
+          (response: any) => {
+            console.log('Article mis à jour avec succès', response);
+            this.OnCloseModal();
+            this.GetTypesArticlesList();
+          },
+          (error: any) => {
+            console.error('Erreur lors de la mise à jour', error);
+          }
+        );
+      } else {
+        this.articleService.createArticle(formValues).then(
+          (response: any) => {
+            this.OnCloseModal();
+            this.GetTypesArticlesList();
+            console.log('Nouvel article créé avec succès', response);
+          },
+          (error: any) => {
+            console.error('Erreur lors de la création', error);
+          }
+        );
+      }
+    }
+  }
+  OnDelete(articleId: any) {
+    ALERT_QUESTION('warning', 'Attention !', 'Voulez-vous supprimer?').then(
+      (res) => {
+        if (res.isConfirmed == true) {
+          this._spinner.show();
+          this.articleService.DeleteArticle([articleId]).then((res: any) => {
+            console.log('DATA:::>', res);
+            this.dataList = res.data;
+            this._spinner.hide();
+          });
+        } else {
+        }
+      }
+    );
+  }
+  GetTypesArticlesList() {
+    let data = {
+      paginate: true,
+      page: 1,
+      limit: 8,
+    };
+    this._spinner.show();
+    this.articleService.GetTypesArticlesList(data).then((res: any) => {
+      console.log('DATA:::>', res);
+      this.dataList = res.data;
+      this._spinner.hide();
+    });
+  }
 }
--- src/app/core/article-service.service.ts
+++ src/app/core/article-service.service.ts
@@ -1,27 +1,98 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ConfigService } from './config-service.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ArticleServiceService {
-  apiUrl:string;
-  constructor(private _http: HttpClient,private configService: ConfigService) { 
+  apiUrl: string;
+  constructor(private _http: HttpClient, private configService: ConfigService) {
     this.apiUrl = this.configService.apiUrl;
   }
 
-  GetGroupeArticleList(data:any){
+  GetTypesArticlesList(data: any) {
     return new Promise((resolve: any, reject: any) => {
-      this._http.get(`${this.apiUrl}/v1/group-article?paginate=${data.paginate}&page=${data.page}&limit=${data.limit}`)
-        .subscribe((res: any) => {
-          console.log(res)
-          resolve(res);
-        }
-          , err => {
+      this._http
+        .get(
+          `${this.apiUrl}/v1/categorie-product?paginate=${data.paginate}&page=${data.page}&limit=${data.limit}`
+        )
+        .subscribe(
+          (res: any) => {
+            console.log(res);
+            resolve(res);
+          },
+          (err) => {
             console.log(err);
             reject(err);
-          });
+          }
+        );
+    });
+  }
+  GetGroupeArticleList(data: any) {
+    return new Promise((resolve: any, reject: any) => {
+      this._http
+        .get(
+          `${this.apiUrl}/v1/group-article?paginate=${data.paginate}&page=${data.page}&limit=${data.limit}`
+        )
+        .subscribe(
+          (res: any) => {
+            console.log(res);
+            resolve(res);
+          },
+          (err) => {
+            console.log(err);
+            reject(err);
+          }
+        );
+    });
+  }
+  DeleteArticle(ids: number[]) {
+    return new Promise((resolve: any, reject: any) => {
+      this._http
+        .delete(`${this.apiUrl}/v1/group-article/bulk-delete`, {
+          body: { ids: ids },
         })
+        .subscribe(
+          (res: any) => {
+            console.log(res);
+            resolve(res);
+          },
+          (err) => {
+            console.log(err);
+            reject(err);
+          }
+        );
+    });
+  }
+  updateArticle(id: number, article: any) {
+    return new Promise((resolve: any, reject: any) => {
+      this._http.put(`${this.apiUrl}/v1/categorie-product/${id}`, article).subscribe(
+        (res: any) => {
+          console.log(res);
+          resolve(res);
+        },
+        (err) => {
+          console.log(err);
+          reject(err);
+        }
+      );
+    });
+  }
+
+  createArticle(article: any) {
+    return new Promise((resolve: any, reject: any) => {
+      this._http.post(`${this.apiUrl}/v1/categorie-product`, article).subscribe(
+        (res: any) => {
+          console.log(res);
+          resolve(res);
+        },
+        (err) => {
+          console.log(err);
+          reject(err);
+        }
+      );
+    });
   }
 }
