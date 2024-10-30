54abb31afa59a716ddc148f0a2b1447782f90817
karamoko vadiahoue <vadiahoue.karamoko@intuitcorporate.com>
2024-10-30 00:01:49 +0000

    debut gestion utilisateur

---
+++ src/app/Features/parametre/utilisateurs/utilisateurs.component.ts
@@ -0,0 +1,28 @@
+import { ChangeDetectorRef, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-utilisateurs',
+  templateUrl: './utilisateurs.component.html',
+  styleUrls: ['./utilisateurs.component.scss']
+})
+export class UtilisateursComponent {
+  public activeTab: string = 'profilutilisateur';
+  loading: boolean = true;
+  selected = [];
+  rows: any = [];
+
+  constructor(private cd: ChangeDetectorRef) {}
+  ngOnInit(): void {
+
+  }
+  activeElement(elt: string) {
+    this.activeTab = elt;
+  }
+  onActivate(event: any) {
+    console.log('Activate Event', event);
+  }
+
+  onSelect(event: any) {
+    console.log('Select Event', event);
+  }
+}
