85b5c42e6da7edebbfc214e7980978812d39ed65
karamoko vadiahoue <vadiahoue.karamoko@intuitcorporate.com>
2025-01-04 18:11:31 +0000

    ecran create revendeurs

--- src/app/Features/partenaires/revendeur/revendeur.component.ts
+++ src/app/Features/partenaires/revendeur/revendeur.component.ts
@@ -1,5 +1,6 @@
 import { Location } from '@angular/common';
 import { ChangeDetectorRef, Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-revendeur',
@@ -7,9 +8,37 @@ import { ChangeDetectorRef, Component } from '@angular/core';
   styleUrls: ['./revendeur.component.scss']
 })
 export class RevendeurComponent {
+  dataList:any[] = []
+  produits:any[] = []
+  depots:any[] = []
+  localites:any[] = []
+  categories:any[] = []
+  clientTypes:any[] = []
+  currentPage: number;
+  rowsPerPage: any;
+    revendeurForm!: FormGroup;
+    operation: string = '';
+    isModalOpen: boolean = false
   constructor(private cd: ChangeDetectorRef, private location: Location) {}
 
   goBack() {
     this.location.back()
   }
+  onPage(event: any) {
+    this.currentPage = event.first / event.rows + 1; // Calculer la page actuelle (1-based index)
+    this.rowsPerPage = event.rows;
+    // this.GetClientOSRList(this.currentPage);
+  }
+  OnCreate(){
+    this.isModalOpen = true
+    this.operation = 'create'
+  }
+  OnCloseModal(){
+    this.isModalOpen = false
+
+  }
+  onSubmit(){}
+  OnEdit(id:number){}
+  OnValidate(id:number){}
+  OnDelete(id:number){}
 }
