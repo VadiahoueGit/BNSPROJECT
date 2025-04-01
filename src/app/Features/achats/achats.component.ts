23fecd9b57273f36fc9dfa20cc19555c7bb2b677
Abou32 <sanogoabou32@gmail.com>
2025-04-01 20:44:06 +0000

    maj

--- src/app/Features/achats/achats.component.ts
+++ src/app/Features/achats/achats.component.ts
@@ -10,35 +10,35 @@ import { Router } from '@angular/router';
 export class AchatsComponent {
   ListItems = [
     {
-      image: 'assets/icon/proforma.png',
+      image: 'assets/icon/supply-chain.png',
       title: 'Commande fournisseurs',
       url: 'feature/achats/commande-fournisseur',
     },
     {
-      image: 'assets/icon/facture.png',
+      image: 'assets/icon/lading.png',
       title: 'Validation commande',
       url:'feature/achats/validation-commande',
     },
     {
-      image: 'assets/icon/commande.png',
+      image: 'assets/icon/delivery-box.png',
       title: 'Réception marchandise',
        url:'feature/achats/reception-marchandise',
     },
     {
-      image: 'assets/icon/commandefree.png',
+      image: 'assets/icon/gift.png',
       title: 'Entrée de gratuité',
       url:'feature/achats/entree-de-gratuite',
     },
     {
-      image: 'assets/icon/delivery.png',
+      image: 'assets/icon/history.png',
       title: 'Historique & gestion des retours',
       url: 'feature/achats/historique-de-gestion-retour',
     },
-    
+
   ]
     constructor(private _router: Router) { }
     ngOnInit(): void {
-  
+
     }
   displayItem(elt: any) {
     console.log(elt)
